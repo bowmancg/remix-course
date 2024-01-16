0eb1f28d979a3080b597759f6291615f9c938590
Chandler Bowman <doctorgrant99@gmail.com>
2024-01-16 11:57:00 -0700

    added use route error and a hydration silencer

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,19 @@
 // import { cssBundleHref } from "@remix-run/css-bundle";
 // import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
-import MainNavigation from '../app/components/MainNavigation'
+import MainNavigation from "../app/components/MainNavigation";
 
-import styles from './styles/main.css';
+import styles from "./styles/main.css";
 
 // export const links: LinksFunction = () => [
 //   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -18,7 +21,7 @@ import styles from './styles/main.css';
 
 export default function App() {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning={true} lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -38,6 +41,38 @@ export default function App() {
   );
 }
 
+export function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+          <title>An error occurred.</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className="error">
+            <h1>An error occurred.</h1>
+            <p>{error.statusText}</p>
+            <p>
+              Back to <Link to="/">safety</Link>.
+            </p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+}
+
 export function links() {
-  return [{rel: 'stylesheet', href: styles}]
+  return [{ rel: "stylesheet", href: styles }];
 }
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,40 +1,60 @@
 import { redirect } from "@remix-run/node";
-import NewNote, {links as newNoteLinks} from "../components/NewNote";
+import NewNote, { links as newNoteLinks } from "../components/NewNote";
 import { getStoredNotes, storeNotes } from "../data/notes";
-import NoteList, {links as noteListLinks} from "../components/NoteList";
-import { useLoaderData } from "@remix-run/react";
+import NoteList, { links as noteListLinks } from "../components/NoteList";
+import {
+  Link,
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 
 export default function NotesPage() {
-    const notes = useLoaderData()
+  const notes = useLoaderData();
 
-    return(
-        <main>
-            <NewNote />
-            <NoteList notes={notes} />
-        </main>
-    )
+  return (
+    <main>
+      <NewNote />
+      <NoteList notes={notes} />
+    </main>
+  );
 }
 
 export async function loader() {
-    const notes = await getStoredNotes()
-    return notes
+  const notes = await getStoredNotes();
+  return notes;
 }
 
-export async function action({request}) {
-    const formData = await request.formData()
-    const noteData = Object.fromEntries(formData)
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
 
-    if (noteData.title.trim().length < 5) {
-        return {message: 'Must be at least 5 characters long.'}
-    }
+  if (noteData.title.trim().length < 5) {
+    return { message: "Must be at least 5 characters long." };
+  }
 
-    const existingNotes = await getStoredNotes()
-    noteData.id = new Date().toISOString()
-    const updatedNotes = existingNotes.concat(noteData)
-    await storeNotes(updatedNotes)
-    return redirect('/notes')
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
 }
 
 export function links() {
-    return [...newNoteLinks(), ...noteListLinks()]
+  return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <main className="error">
+        <h1>An error occurred.</h1>
+        <p>{error.message}</p>
+        <p>
+          Back to <Link to="/">safety</Link>.
+        </p>
+      </main>
+    );
+  }
 }
